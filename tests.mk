8793b08a6d5ed47e66aa1eec7365113245f09a0d
David Pirotte <david@altosw.be>
2015-08-26 15:42:19 -0300

    fix make distcheck
    * tests.mk: Adding GUILE_FLAGS, TESTS_ENVIRONMENT to use
      $(SCM_LOG_COMPILER), plus some other [cosmetic] changes.  Also,
      GUILE=$(GUILE_GNOME) [which sets %load-path as it should wrt
      required guile-gnome's modules, even if (gnome-2) is/was not
      imported first.

--- tests.mk
+++ tests.mk
@@ -1,45 +1,59 @@
-# guile-gnome
-# Copyright (C) 2007 Free Software Foundation, Inc.
 
-# This program is free software; you can redistribute it and/or    
-# modify it under the terms of the GNU General Public License as   
-# published by the Free Software Foundation; either version 2 of   
-# the License, or (at your option) any later version.              
+####
+#### Copyright (C) 2007, 2015 Free Software Foundation, Inc.
 
-# This program is distributed in the hope that it will be useful,  
-# but WITHOUT ANY WARRANTY; without even the implied warranty of   
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    
-# GNU General Public License for more details.                     
+#### This file is part of Guile-Clutter.
+#### A Guile-Clutter of Really Important Procedures.
 
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, contact:
+#### Guile-Clutter is free software: you can redistribute it and/or
+#### modify it under the terms of the GNU General Public License as
+#### published by the Free Software Foundation, either version 3 of
+#### the License, or (at your option) any later version.
 
-# Free Software Foundation           Voice:  +1-617-542-5942
-# 59 Temple Place - Suite 330        Fax:    +1-617-542-2652
-# Boston, MA  02111-1307,  USA       gnu@gnu.org
+#### Guile-Clutter is distributed in the hope that it will be useful, but
+#### WITHOUT ANY WARRANTY; without even the implied warranty of
+#### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#### General Public License for more details.
+
+#### You should have received a copy of the GNU General Public License
+#### along with Guile-Clutter.  If not, see <http://www.gnu.org/licenses/>.
+####
 
 
-#
-# Makefile snippet
-#
-EXTRA_DIST = wrapset.api wrapset.scm
+EXTRA_DIST = 		\
+	wrapset.api	\
+	wrapset.scm
 
 wrapset_stem = set-wrapset_stem-in-your-makefile
 
 top_module_name = (gnome $(wrapset_stem))
-gw_module_name = (gnome gw $(wrapset_stem))
-extra_module_names =
-wrapset_modules = ($(top_module_name) $(gw_module_name) $(extra_module_names))
-WRAPSET_TESTS_ENV = WRAPSET_MODULES="$(wrapset_modules)" WRAPSET_API_FILE=$(srcdir)/wrapset.api
-DEV_ENV = $(top_builddir)/dev-environ
-GUILE=$(if $(findstring glib,$(AG_PACKAGES)),guile,guile-gnome-$(API_VERSION))
 
-TESTS_ENVIRONMENT=$(WRAPSET_TESTS_ENV) $(DEV_ENV)
+gw_module_name = (gnome gw $(wrapset_stem))
+
+extra_module_names =
+
+wrapset_modules =		\
+	($(top_module_name)	\
+	$(gw_module_name)	\
+	$(extra_module_names))
+
+WRAPSET_TESTS_ENV = \
+	WRAPSET_MODULES="$(wrapset_modules)" \
+	WRAPSET_API_FILE=$(srcdir)/wrapset.api
+
+DEV_ENV = $(top_builddir)/dev-environ
+GUILE = $(GUILE_GNOME)
+GUILE_FLAGS = --debug
 SCM_LOG_COMPILER = $(GUILE) $(GUILE_FLAGS) -e main -s
+
+TESTS_ENVIRONMENT = \
+	$(WRAPSET_TESTS_ENV) $(DEV_ENV) $(SCM_LOG_COMPILER)
+
 TEST_EXTENSIONS = .scm
 
 wrapset.api.update:
-	$(WRAPSET_TESTS_ENV) $(DEV_ENV)	$(GUILE) -e update-api -s $(srcdir)/wrapset.scm
+	$(WRAPSET_TESTS_ENV) $(DEV_ENV) \
+	$(GUILE) $(GUILE_FLAGS) -e update-api -s $(srcdir)/wrapset.scm
 
 %.check: %
 	$(TESTS_ENVIRONMENT) $(srcdir)/$*
